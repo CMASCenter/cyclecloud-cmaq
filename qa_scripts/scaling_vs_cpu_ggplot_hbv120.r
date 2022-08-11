9e402db88ed05da65465bd3fb539776253141c8a
lizadams <lizadams@email.unc.edu>
2022-08-11 13:13:48 -0400

    update

--- qa_scripts/scaling_vs_cpu_ggplot_hbv120.r
+++ qa_scripts/scaling_vs_cpu_ggplot_hbv120.r
@@ -17,11 +17,11 @@ bad_rows <-
     is.na(csv_data$Efficiency) |
     is.na(csv_data$MS_Opt_Pin)
 p1 <- ggplot(csv_data[!bad_rows,], aes(y=Scaling, x=CPUs, color=MS_Opt_Pin, size=ComputeNode)) +
-    geom_point() + ggtitle("2 Day Benchmark Scaling versus CPU using HBv120") +  xlim(0,400) + ylim(0,55) + stat_smooth(method="lm")
+    geom_point() + ggtitle("2 Day Benchmark Scaling versus CPU using HBv120") +  xlim(0,400) + ylim(0,55)
 
 
 p2 <- ggplot(csv_data[!bad_rows,], aes(y=Efficiency, x=CPUs, color=MS_Opt_Pin, size=ComputeNode)) +
-    geom_point() + ggtitle("2 Day Benchmark Parallel Efficiency versus CPU using HBv120") + stat_smooth(method="lm")
+    geom_point() + ggtitle("2 Day Benchmark Parallel Efficiency versus CPU using HBv120")
 
 # Display both charts side by side thanks to the patchwork package
 p1 + geom_abline(intercept = 0, slope = .0552) + p2
