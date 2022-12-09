34a41d6c434613697b5f5fb47f9dc36da897a881
lizadams <lizadams@email.unc.edu>
2022-12-09 20:40:09 +0000

    update diagram

--- docs/diagrams/nested_clusters_cyclecloud.py
+++ docs/diagrams/nested_clusters_cyclecloud.py
@@ -5,8 +5,10 @@ from diagrams.aws.integration import SQS
 from diagrams.aws.storage import S3, EBS, FSx
 from diagrams.aws.devtools import CLI
 from diagrams.azure.compute import Disks
+from diagrams.azure.storage import BlobStorage
+from diagrams.azure.storage import StorageSyncServices
 
-with Diagram("Microsoft Azure Minimum Viable Product", show=False):
+with Diagram("Microsoft Azure Cycle Cloud", show=False):
     source = CLI("Azure")
 
     with Cluster("Cycle Cloud"):
@@ -15,16 +17,28 @@ with Diagram("Microsoft Azure Minimum Viable Product", show=False):
         
 
         with Cluster("Scheduler Node"):
-            head = [EC2("F2sV2")]
+            head = [EC2("D4ds v5 (4 vcpus)")]
 
-        with Cluster("HBv3 - 120CPU/compute node"):
+        with Cluster("HBv3 - 120CPU/compute node; Scalable based SLURM request"):
             workers = [EC2("HBv3-120"),
                        EC2("HBv3-120"),
                         EC2("HBv3-120")]
-        volume1 = Disks("/shared - 1.0TB")
+        with Cluster("Shared"):
+             sched = [
+                     EC2("Scheduler Node"), 
+                     Disks("Shared - 1.0TB")]
+        with Cluster("Data"):
+             volume1 = [
+                     Disks("data-1.0TB")]
+        with Cluster("Lustre"):
+             volume2 = [ 
+                     Disks("lustre-64TB")]
+        mnt = StorageSyncServices("Mount Point")
 
-
-    store = S3("S3 Bucket")
+    store = BlobStorage("Blob Storage")
 
     source >> head >> queue >> workers
-    workers >> volume1 >> store
+    workers >> mnt 
+    mnt >> sched  >> store
+    mnt >> volume1 >> store
+    mnt >> volume2 >> store
