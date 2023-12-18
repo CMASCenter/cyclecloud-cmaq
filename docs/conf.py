7ca7043bda2a0e89b9c556b0cbec4c9e9231d2f0
lizadams <lizadams@email.unc.edu>
2023-12-18 18:51:15 -0500

    update

--- docs/conf.py
+++ docs/conf.py
@@ -34,6 +34,7 @@ extensions = ["myst_parser",
               "sphinx.ext.duration",
               "sphinx.ext.autosectionlabel",
               "sphinx.ext.autodoc",
+              "sphinx_copybutton",
               "nbsphinx", "edit_on_github" #MyST-NB
  
 ]
