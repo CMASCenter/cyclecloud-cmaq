cdb78cc8f5b9eae13b77aa46912b7f5c59d815ab
lizadams <lizadams@email.unc.edu>
2024-06-03 15:58:07 -0400

    update

--- qa_scripts/CMAQ_main_log_pin_nopin_combined.R
+++ qa_scripts/CMAQ_main_log_pin_nopin_combined.R
@@ -3,26 +3,23 @@
 # Affiliation: US EPA Office of Research and Development
 # This script assumes that the log files are located in ./CCTM/scripts as output by the CMAQ run script 
 # These are the single output files, not the CTM_LOG files found in the $OUTDIR/LOGS directory
-# Simulation parameters
-
 library(RColorBrewer)
 library(stringr)
-#sens.dir  <- '/shared/cyclecloud-cmaq/run_scripts/HB120v3_singleVM/'
-sens.dir  <- '/proj/ie/proj/CMAS/CMAQ/pcluster-cmaq/run_scripts/hpc6a_ebs_fsx/'
-base.dir  <- '/proj/ie/proj/CMAS/CMAQ/cyclecloud-cmaq/run_scripts/storage_logs/'
-#files   <- dir(sens.dir, pattern ='run_cctm_2016_12US2.96pe.1x96.cyclecloud.nvme.pin.log')
-files   <- dir(sens.dir, pattern ='run_cctmv5.3.3_Bench_2016_12US2.96.12x8pe.2day.cyclecloud.shared.codemod.pin.log')
-files <- c('run_cctmv5.3.3_Bench_2016_12US2.hpc6a.48xlarge.96.1x96.12x8pe.2day.pcluster.shared.pin.codemod_liz.writevar.log','run_cctmv5.3.3_Bench_2016_12US2.hpc6a.48xlarge.96.1x96.12x8pe.2day.pcluster.fsx.pin.codemod_liz.writevar.log')
-#b.files <- dir(base.dir,pattern='run_cctmv5.3.3_Bench_2016_12US2.96.12x8pe.2day.cyclecloud.shared.codemod.nopin.redo.log')
-b.files <- c('run_cctmv5.3.3_Bench_2016_12US2.96.12x8pe.2day.cyclecloud.shared.codemod.pin.log','run_cctmv5.3.3_Bench_2016_12US2.96.12x8pe.2day.cyclecloud.data.codemod.pin.log','run_cctmv5.3.3_Bench_2016_12US2.96.12x8pe.2day.cyclecloud.lustre3.codemod.pin.log','run_cctm_2016_12US2.96pe.1x96.cyclecloud.nvme.pin.log')
+
+sens.dir  <- '/proj/ie/proj/CMAS/CMAQ/pcluster-cmaq/run_scripts/hpc6a_combined/'
+base.dir  <- '/proj/ie/proj/CMAS/CMAQ/cyclecloud-cmaq/run_scripts/HB120v3_pin_testing/lustre/logs/'
+files   <- c('run_cctmv5.3.3_Bench_2016_12US2.hpc6a.48xlarge.576.6x96.24x24pe.2day.pcluster.shared.nopin.codemod.log','run_cctmv5.3.3_Bench_2016_12US2.hpc6a.48xlarge.576.6x96.24x24pe.2day.pcluster.shared.pin.codemod.log','run_cctmv5.3.3_Bench_2016_12US2.hpc6a.48xlarge.576.6x96.24x24pe.2day.pcluster.log','run_cctmv5.3.3_Bench_2016_12US2.hpc6a.48xlarge.576.6x96.24x24pe.2day.pcluster.fsx.pin.codemod.3.log')
+#b.files <- dir(base.dir,pattern='run_cctmv5.3.3_Bench_2016_12US2.576.24x24pe.2day.cyclecloud.shared.codemod.nopin.redo.log')
+b.files <- c('run_cctmv5.3.3_Bench_2016_12US2.576.24x24pe.2day.cyclecloud.lustre.codemod.nopin.redo.log','run_cctmv5.3.3_Bench_2016_12US2.576.24x24pe.2day.cyclecloud.lustre.codemod.pin.ccc.log')
 #Compilers <- c('intel','gcc','pgi')
 Compilers <- c('gcc')
 # name of the base case timing. I am using the current master branch from the CMAQ_Dev repository.
 # The project directory name is used for the sensitivity case.
 #base.name <- c('data_pin','lustre_pin','shared_pin')
-base.name <- c('data','lustre','nvme')
-sens.name <- c('shared')
+base.name <- c('l_nopin_hbv120','l_pin_hbv120')
+sens.name <- c('s_nopin_hpc6a','s_pin_hpc6a','l_nopin_hpc6a','l_pin_hpc6a')
 
+# Simulation parameters
 
 
 
@@ -36,13 +33,9 @@ sens.name <- c('shared')
 #n.lev     <- length(sens.name[[1]])
 #sens.name <- sens.name[[1]][n.lev]
 all.names <- NULL
-for(i in 1:length(files)){
    all.names <- append(all.names,sens.name)
-}
 files <- paste(sens.dir,files,sep="")
-for(i in 1:length(files)){
    all.names <- append(all.names,base.name)
-}
 files <- append(files,paste(base.dir,b.files,sep=''))
 for( comp in Compilers) {
    bar.data <- NULL
@@ -92,9 +85,9 @@ for( comp in Compilers) {
    my.colors <- brewer.pal(12, "Paired")
    #my.colors <- terrain.colors(length(n.proc))
    xmax <- dim(bar.data)[2]*1.5
-   png(file = paste('hb120v3_1nodes_96pe_all_filesystem_',comp,'_all',sens.name,'_',base.name,'.png',sep=''), width = 1024, height = 768, bg='white')
+   png(file = paste('hb120v3_5nodes_576pe_',comp,'_all',sens.name,'_',base.name,'.png',sep=''), width = 1024, height = 768, bg='white')
   # png(file = paste(comp,'_',sens.name,'.png',sep=''), width = 1024, height = 768, bg='white')
-   barplot(bar.data, main = 'Process Timing on all filesystems with 1 node, 96 cpus/node on HB120v3 with pinning',names.arg = b.names,ylab='seconds', col = my.colors, legend = n.proc.plot, xlim = c(0.,xmax),ylim = c(0.,6000.))
+   barplot(bar.data, main = 'Process Timing on /shared and /lustre using 6 nodes with 96 cpus/node on HB120v3 and HPC6a without and with pinning',names.arg = b.names,ylab='seconds', col = my.colors, legend = n.proc.plot, xlim = c(0.,xmax),ylim = c(0.,2500.))
    box()
    dev.off()
  
