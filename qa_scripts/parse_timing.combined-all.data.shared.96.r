9506e274e72e7f254f5aa3aa9aeb88eebfb69475
lizadams <lizadams@email.unc.edu>
2024-06-12 12:22:47 -0400

    update

--- qa_scripts/parse_timing.combined-all.data.shared.96.r
+++ qa_scripts/parse_timing.combined-all.data.shared.96.r
@@ -89,7 +89,7 @@ for( comp in Compilers) {
    xmax <- dim(bar.data)[2]*1.2
    png(file = paste('hb120v3_combined_data_shared_1-6nodes_',comp,'_all',sens.name,'_',base.name,'.png',sep=''), width = 1024, height = 768, bg='white')
   # png(file = paste(comp,'_',sens.name,'.png',sep=''), width = 1024, height = 768, bg='white')
-   barplot(bar.data, main = 'Process Timing on Filesystems using 1-6 nodes with 96 cpus/node on HB120v3 with pinning',names.arg = b.names,ylab='seconds', xlab="Cores,Filesystem(shared,shared,lustre-150,lustre-250)", col = my.colors, legend = n.proc.plot, xlim = c(0.,xmax),ylim = c(0.,6000.))
+   barplot(bar.data, main = 'Process Timing on Filesystems using 1-6 nodes with 96 cpus/node on HB120v3 with pinning',names.arg = b.names,ylab='seconds', xlab="Cores,Filesystem(shared,data,lustre-150,lustre-250)", col = my.colors, legend = n.proc.plot, xlim = c(0.,xmax),ylim = c(0.,6000.))
       # Add abline
             text(x = .2, y = 5900, "shared")
           abline(v=c(7.3) , col="black", lwd=3, lty=2)
